11921180031f77ab117abbffd267ad1b136eae0f
sjoongh <az45687@naver.com>
2022-03-15 12:27:22 +0900

    foreach

--- src/main/webapp/js/day32.js
+++ src/main/webapp/js/day32.js
@@ -1,3 +1,15 @@
 import random from './getRandom';
 
 console.log(123);
+
+for (let i =0; i < 3; i+= 1) {
+	const li = document.createElement('li');
+	li.textContent = `사과${i+1}`;
+	ulEl.appendChild(li);
+}
+
+// 찾은 요소를 반복해서 함수 실행
+boxEls.forEach(function(boxEl, index) {
+	console.log(index, boxEl);
+	boxEl.classList.add(`order${index+1}`);
+}) 
